3a806a3dd3c6aecd30ebebb135167a860fe9a6ac
viethz21 <nquocviet1210@gmail.com>
2026-03-12 00:58:49 +0700

    Add project files.

---
+++ BaiThucHanhC#/frm_Login.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace BaiThucHanhC_
+{
+    partial class frm_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LbTitle = new System.Windows.Forms.Label();
+            this.LbUser = new System.Windows.Forms.Label();
+            this.LbPass = new System.Windows.Forms.Label();
+            this.txt_User = new System.Windows.Forms.TextBox();
+            this.txt_Pass = new System.Windows.Forms.TextBox();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LbTitle
+            // 
+            this.LbTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbTitle.Location = new System.Drawing.Point(17, 49);
+            this.LbTitle.Name = "LbTitle";
+            this.LbTitle.Size = new System.Drawing.Size(492, 40);
+            this.LbTitle.TabIndex = 0;
+            this.LbTitle.Text = "Đăng nhập hệ thống";
+            this.LbTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LbUser
+            // 
+            this.LbUser.AutoSize = true;
+            this.LbUser.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbUser.Location = new System.Drawing.Point(12, 165);
+            this.LbUser.Name = "LbUser";
+            this.LbUser.Size = new System.Drawing.Size(146, 24);
+            this.LbUser.TabIndex = 1;
+            this.LbUser.Text = "Tên đăng nhập:";
+            // 
+            // LbPass
+            // 
+            this.LbPass.AutoSize = true;
+            this.LbPass.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbPass.Location = new System.Drawing.Point(12, 212);
+            this.LbPass.Name = "LbPass";
+            this.LbPass.Size = new System.Drawing.Size(100, 24);
+            this.LbPass.TabIndex = 2;
+            this.LbPass.Text = "Mật khẩu:";
+            // 
+            // txt_User
+            // 
+            this.txt_User.Location = new System.Drawing.Point(174, 167);
+            this.txt_User.Name = "txt_User";
+            this.txt_User.Size = new System.Drawing.Size(250, 22);
+            this.txt_User.TabIndex = 1;
+            this.txt_User.TextChanged += new System.EventHandler(this.txt_User_TextChanged);
+            // 
+            // txt_Pass
+            // 
+            this.txt_Pass.Location = new System.Drawing.Point(174, 214);
+            this.txt_Pass.Name = "txt_Pass";
+            this.txt_Pass.PasswordChar = '*';
+            this.txt_Pass.Size = new System.Drawing.Size(250, 22);
+            this.txt_Pass.TabIndex = 2;
+            this.txt_Pass.TextChanged += new System.EventHandler(this.txt_Pass_TextChanged);
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btn_Login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Login.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_Login.Location = new System.Drawing.Point(115, 307);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(110, 35);
+            this.btn_Login.TabIndex = 3;
+            this.btn_Login.Text = "Đăng nhập";
+            this.btn_Login.UseVisualStyleBackColor = false;
+            this.btn_Login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(303, 307);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(110, 35);
+            this.btn_Exit.TabIndex = 4;
+            this.btn_Exit.Text = "Hủy";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 395);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.txt_Pass);
+            this.Controls.Add(this.txt_User);
+            this.Controls.Add(this.LbPass);
+            this.Controls.Add(this.LbUser);
+            this.Controls.Add(this.LbTitle);
+            this.MinimizeBox = false;
+            this.Name = "frm_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login - Quản lý sinh viên";
+            this.Load += new System.EventHandler(this.frm_Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LbTitle;
+        private System.Windows.Forms.Label LbUser;
+        private System.Windows.Forms.Label LbPass;
+        private System.Windows.Forms.TextBox txt_User;
+        private System.Windows.Forms.TextBox txt_Pass;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.Button btn_Exit;
+    }
+}
+
---
+++ BaiThucHanhC#/frm_Login.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiThucHanhC_
+{
+    public partial class frm_Login : Form
+    {
+        public frm_Login()
+        {
+            InitializeComponent();
+        }
+        private void frm_Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_User_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            string username = txt_User.Text;
+            string password = txt_Pass.Text;
+
+            if (username == "1" && password == "1")
+            {
+                MessageBox.Show("Đăng nhập thành công!");
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại. Vui lòng kiểm tra lại tên đăng nhập và mật khẩu.");
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txt_Pass_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
